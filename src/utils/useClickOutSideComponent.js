076668f6ba9c82d3a16688321e0851b4e8e4fe25
catbouce <75095386+catbouce@users.noreply.github.com>
2023-06-19 15:09:44 -0700

    feat: Add postcss, autoprefixer, and tailwindcss packages

---
+++ src/utils/useClickOutSideComponent.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+export const useClickOutSideComponent = (ref, setIsOpen) => {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        const hamburger = document.getElementById("hamburger");
+        if (hamburger && hamburger.contains(event.target)) {
+          return;
+        }
+
+        setIsOpen(false);
+        return;
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside);
+    window.addEventListener("scroll", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
+      window.removeEventListener("scroll", handleClickOutside);
+    };
+  }, [ref, setIsOpen]);
+};
